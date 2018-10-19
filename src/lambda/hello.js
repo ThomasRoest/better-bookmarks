d668efc7d5701240ce4fe46f2a442c3a1ec16498
thomasroest <user@example.com>
2018-10-19 13:30:17 +0200

    udpate lambda function test

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -3,12 +3,12 @@ const axios = require("axios");
 
 // const { JSDOM } = jsdom;
 
-const URL = "https://github.com";
+const URL = "https://jsonplaceholder.typicode.com/posts";
 
 exports.handler = async (event, context) => {
   const response = await axios.get(URL);
   // const dom = new JSDOM(response.data);
-  // // console.log(response);
+  // // // console.log(response);
   // const title = dom.window.document.querySelector("title").textContent;
   // console.log(title);
   // const data = JSON.parse(event.body);
